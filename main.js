adb6d9cc490e3cf6b9d6cb496c55d2d75d141a8a
Coding WIth Ahmed <155929233+ahmedprogrammer2005@users.noreply.github.com>
2024-03-06 02:05:16 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+// Assignment 2 (Variables and Strings)
+
+// Answer 1
+var username;
+
+// Answer 2
+var myName = "Ahmed";
+
+// Answer 3
+var message;
+message = "Hello World";
+alert(message);
+
+//  Answer 4
+var studentName = "Ahmed";
+var studentAge = 20;
+var studentGrade = "A";
+
+alert("Name: " + studentName);
+alert("Age: " + studentAge);
+alert("Grade: " + studentGrade);
+
+// Answer 5
+var word = "PIZZA";
+
+alert(word);
+alert(word.slice(0, -1));
+alert(word.slice(0, -2));
+alert(word.slice(0, -3));
+alert(word.slice(0, -4));
+
+// Answer 6
+var email = "example@example.com";
+alert("My email address is: " + email);
+
+// Answer 7
+var book = "A smarter way to learn JavaScript";
+alert("I am currently reading the book: '" + book + "'");
+
+//  Answer 8
+document.write('Yes, I can Write Here');
+// Answer 9
+var specialString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(specialString);
